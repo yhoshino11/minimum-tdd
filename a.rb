347eb42c4d44227088022a6e1a85f3502ac5e6e1
Yu Hoshino <yhoshino11@gmail.com>
2015-04-21 17:23:39 +0900

    Initial Commit

---
+++ a.rb
@@ -0,0 +1,25 @@
+class Person
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+end
+
+require 'rspec'
+
+RSpec.describe Person do
+  context 'name' do
+    it 'shows' do
+      person = Person.new('Steve Jobs')
+      expect(person.name).to eq('Steve Jobs')
+    end
+  end
+end
